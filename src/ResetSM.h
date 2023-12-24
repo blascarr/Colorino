b86cf974d76ef405b60e628286666636ba1c7cfe
blascarr <adrian.blascarr@gmail.com>
2023-12-24 14:17:58 +0100

    Reset model include in State Machine from Serial

---
+++ src/ResetSM.h
@@ -0,0 +1,25 @@
+#ifndef _RESETSM_H
+#define _RESETSM_H
+
+#define resetSMinterval 40
+bool resetSMSignal = false;
+void resetSMRun() {
+	if (Serial.available()) {
+		char data = Serial.read();
+		Serial.print("SIGNAL : ");
+		Serial.print(data);
+		if (data == 'R') {
+			resetSMSignal = true;
+			Serial.print(" - ");
+			Serial.println(resetSMSignal);
+		}
+		if (data == 'S') {
+			Serial.print(" - Current SM Signal: ");
+			Serial.println(resetSMSignal);
+		}
+	}
+}
+
+Ticker resetSMTicker(resetSMRun, resetSMinterval, 0, MILLIS);
+
+#endif
